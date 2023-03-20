076028614f1bd6ddfc02735b27e1a6bf6152f47f
Nicholas Galante <nicholasgalante94@gmail.com>
2023-03-19 21:58:18 -0400

    Add get method for flashcards

--- project-server/app/controllers/application_controller.rb
+++ project-server/app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
   get "/study_sets" do
     study_sets = StudySet.all 
     study_sets.to_json
   end
 
-  
+  get "/flashcards" do
+    flashcards = Flashcard.all 
+    flashcards.to_json
+  end
+
+  get "/study_sets/:id" do
+    study_set = StudySet.find(params[:id])
+    study_set.to_json
+  end
 
 end
