8200cd4f1dd6a79d77b6ca44633d132147ee9f98
Nicholas Galante <nicholasgalante94@gmail.com>
2023-03-20 11:06:52 -0400

    Create NavBar and fetch study sets

--- project-client/src/components/App.js
+++ project-client/src/components/App.js
@@ -1,7 +1,21 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+
 function App() {
+  const [studySets, setStudySets] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:9292/study_sets")
+      .then(res => res.json())
+      .then(data => setStudySets(data))
+  })
+
+
+
   return (
     <div>
-      Hello World!
+      <NavBar/>
+      Your Study Sets: 
     </div>
   );
 }
