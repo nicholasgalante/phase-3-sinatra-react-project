241554abf293dd889658e1e06d8a0774747e67b7
Nicholas Galante <nicholasgalante94@gmail.com>
2023-03-22 10:11:54 -0400

    Upgrade React Dom Routes to latest version

--- project-client/src/components/App.js
+++ project-client/src/components/App.js
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
 import StudySetCard from "./StudySetCard";
 
 function App() {
-  const [studySets, setStudySets] = useState([])
+  const [studySets, setStudySets] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://localhost:9292/study_sets")
-      .then(res => res.json())
-      .then(data => setStudySets(data))
-  })
+      .then((res) => res.json())
+      .then((data) => setStudySets(data));
+  });
 
-  const displayStudySets = studySets.map(set => {
-    return <StudySetCard key={set.id} set={set}/>
-  })
+  const displayStudySets = studySets.map((set) => {
+    return <StudySetCard key={set.id} set={set} />;
+  });
 
   return (
     <div>
-      <NavBar/>
-          Your Study Sets: 
-          {displayStudySets}
-
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={displayStudySets} />
+          <Route path="/hola" element={"Hola Mundo"}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
