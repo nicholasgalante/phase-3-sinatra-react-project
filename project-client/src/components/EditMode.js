a8c5543e7907df53d3298de05b98998d76f43ec5
Nicholas Galante <nicholasgalante94@gmail.com>
2023-03-23 09:27:58 -0400

    Fetch study set data using useParams

--- project-client/src/components/EditMode.js
+++ project-client/src/components/EditMode.js
@@ -1,9 +1,21 @@
-function EditMode(){
-   return(
-      <>
-         Hola Mundo
-      </>
-   )
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function EditMode() {
+  const [activeStudySet, setActiveStudySet] = useState([]);
+
+  const { title } = activeStudySet;
+  const { setId } = useParams();
+
+  console.log(setId);
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/study_sets/${setId}`)
+      .then((res) => res.json())
+      .then((data) => setActiveStudySet(data));
+  }, [setId]);
+
+  return <>{title}</>;
 }
 
 export default EditMode;
