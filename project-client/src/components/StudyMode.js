24010e5b8274d6e7df3699a4a19b71285f9c3523
Nicholas Galante <nicholasgalante94@gmail.com>
2023-04-02 15:11:20 -0400

    Add index count and handleNext function

--- project-client/src/components/StudyMode.js
+++ project-client/src/components/StudyMode.js
@@ -1,40 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-function StudyMode({studySets}) {
+function StudyMode({ studySets }) {
   const [activeStudySet, setActiveStudySet] = useState({});
   const [index, setIndex] = useState(0);
 
   const { setId } = useParams();
 
-  useEffect(()=> {
-   setActiveStudySet(studySets.find(set => set.id == setId))
-  },[studySets])
+  useEffect(() => {
+    setActiveStudySet(studySets.find((set) => set.id == setId));
+  }, [studySets]);
 
+  console.log("ACTIVE STUDY SET", activeStudySet);
 
+  function handleNext() {
+    setIndex((prevIndex) => prevIndex + 1);
+  }
 
-  
-  console.log("ACTIVE STUDY SET", activeStudySet)
+  if (!activeStudySet) {
+    return "loading!";
+  } //is this good practice?
 
-
-//   useEffect(() => {
-//     fetch(`http://localhost:9292/study_sets/${setId}`)
-//       .then((res) => res.json())
-//       .then((data) => setActiveStudySet(data));
-//   }, [setId]);
-
-//   console.log("ACTIVE STUDY SET", activeStudySet)
-
-
-  return <>
-  You are now in study mode!
-  {activeStudySet.title}
-  Displaying card # 
-  {index}
-
-  <button>Previous</button>
-  <button>Next</button>
-  </>;
+  return (
+    <>
+      You are now in study mode!
+      {activeStudySet.title}
+      Displaying card #{index}
+      <button>Previous</button>
+      <button onClick={handleNext}>Next</button>
+    </>
+  );
 }
 
 export default StudyMode;
